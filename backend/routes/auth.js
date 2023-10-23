0e84a4455c4a262afde092ec60641e0c89db7524
Divyansh Raj <rajdivyansh2000@gmail.com>
2023-10-23 18:07:55 +0530

    refactor backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,25 +13,31 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // If there are errors, return bad request and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: "Please enter a unique email",
-          message: err.message,
-        });
+    //Check whether the user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
+
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Error Occurred");
+    }
   }
 );
 
