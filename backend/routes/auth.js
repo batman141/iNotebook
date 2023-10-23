97c1c90447e23fbe25285c4da1090afc56e87855
Divyansh Raj <rajdivyansh2000@gmail.com>
2023-10-23 17:27:40 +0530

    using backend to create users

---
+++ backend/routes/auth.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+
+// Create a User using: POST "/api/auth/createuser". Doesn't require Login
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be at least 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    // If there are errors, return bad request and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: "Please enter a unique email",
+          message: err.message,
+        });
+      });
+  }
+);
+
+module.exports = router;
