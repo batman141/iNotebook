748014f49199c65d637d30ae5840cbde7226c2de
Divyansh Raj <rajdivyansh2000@gmail.com>
2024-04-08 19:24:39 +0530

    give Navbar and Home more structure

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,7 @@
-import React, { useEffect } from "react";
-import { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
+import React from "react";
 
 const About = () => {
-  const a = useContext(noteContext);
-
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
-
-  return (
-    <div>
-      This is about {a.state.name} from class {a.state.class}
-    </div>
-  );
+  return <div>This is About</div>;
 };
 
 export default About;
