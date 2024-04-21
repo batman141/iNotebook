ad440b7ddb14ecadc810f6a16ec447bb8d6ac6c3
Divyansh Raj <rajdivyansh2000@gmail.com>
2024-04-22 01:34:47 +0530

    Add Login component

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -64,6 +64,8 @@ router.post(
     body("password", "Password cannot be blank").exists(),
   ],
   async (req, res) => {
+    // shows whether the user logged in successfully
+    let success = false;
     // If there are errors, return bad request and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -73,21 +75,25 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (!user) {
+        success = false;
         return res
           .status(400)
           .json({ error: "Please try to login with correct credentials" });
       }
       const pwdCompare = await bcrypt.compare(password, user.password);
       if (!pwdCompare) {
-        return res
-          .status(400)
-          .json({ error: "Please try to login with correct credentials" });
+        success = false;
+        return res.status(400).json({
+          success,
+          error: "Please try to login with correct credentials",
+        });
       }
       const data = {
         user: { id: user.id },
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json({ authtoken });
+      success = true;
+      res.json({ success, authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
---
+++ src/components/Login.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  // for saving auth-token
+  let navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    //TODO
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      // Save the authtoken and redirect
+      localStorage.setItem("token", json.authtoken);
+      navigate("/");
+    } else {
+      alert("Invalid credentials");
+    }
+  };
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            name="email"
+            onChange={onChange}
+            value={credentials.email}
+          />
+          <div id="emailHelp" className="form-text">
+            Please enter valid email address
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            onChange={onChange}
+            value={credentials.password}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
